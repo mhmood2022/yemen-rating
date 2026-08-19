93f1b258ad5d30fa17c0bf632568e947a7d53607
mhmood2022 <mhmood7015@gmail.com>
2026-08-20 00:44:15 +0300

    Fix: update supabase key in js/supabase.js

--- js/supabase.js
+++ js/supabase.js
@@ -2,7 +2,7 @@
 // ⚠️ ضع مفاتيحك الحقيقية هنا ⚠️
 const SUPABASE_URL = 'https://wkdqeghotlipciqiytuj.supabase.co';
 
-const SUPABASE_KEY = 'sb_publishable_4StPj676njVXlDg4UUwaJg_xSqhsIuT';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrZHFlZ2hvdGxpcGNpcWl5dHVqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY5MDM4NzEsImV4cCI6MjEwMjQ3OTg3MX0.ahqq5okKMXMxuI-8sArjxcVIpPDRmX20mhscs8BaCTE';
 
 let sb = null;
 let initPromise = null;
